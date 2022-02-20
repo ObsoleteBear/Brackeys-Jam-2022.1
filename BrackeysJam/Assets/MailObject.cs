ed315d0bb918bf7361064854f381265df7ac4f61
ObsoleteBear <pauldavidreaves@gmail.com>
2022-02-20 14:35:13 -0600

    MailColors (gitignore too)

---
+++ BrackeysJam/Assets/MailObject.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MailObject : MonoBehaviour
+{
+    public string color;
+    public bool hasStamp;
+    public string shape;
+
+    public SpriteRenderer stamp;
+
+    public void Update()
+    {
+        switch (color)
+        {
+            case "Blue":
+                gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+                break;
+            case "Red":
+                gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+                break;
+            case "Yellow":
+                gameObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+                break;
+            default:
+                break;
+        }
+
+        if (hasStamp)
+        {
+            stamp.enabled = true;
+        }
+        else
+        {
+            stamp.enabled = false;
+        }
+    }
+}
