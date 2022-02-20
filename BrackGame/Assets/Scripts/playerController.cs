d94feff4e5cd1b6b7b733c69690634ddaecab237
Diamondsword0 <diamondsword19@gmx.com>
2022-02-20 13:10:45 -0600

    deez4
    player movement thing

--- BrackGame/Assets/Scripts/playerController.cs
+++ BrackGame/Assets/Scripts/playerController.cs
@@ -7,10 +7,13 @@ public class playerController : MonoBehaviour
     public float speed;
     public Rigidbody2D rb;
     Vector2 movement;
+    private float deez;
+    private SpriteRenderer sprite;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     
@@ -18,11 +21,26 @@ public class playerController : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if(deez > 0)
+        {
+            sprite.flipX = false;
+        } 
+        if(deez < 0)
+        {
+            sprite.flipX = true;
+        }
+            
     }
+    
 
     private void FixedUpdate()
     {
+        deez = Input.GetAxis("Horizontal");
         movement.Normalize();
         rb.velocity = new Vector2(movement.x * speed * Time.deltaTime, movement.y * speed * Time.deltaTime);
+        print(deez);
     }
+
+    
 }
