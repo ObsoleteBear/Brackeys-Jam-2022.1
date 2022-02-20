489bb3fdd04021ec49f94b00a6f26bb88dbb3034
Diamondsword0 <diamondsword19@gmx.com>
2022-02-20 13:32:32 -0600

    player letter

---
+++ BrackGame/Assets/Scripts/letterScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class letterScript : MonoBehaviour
+{
+    //Put this on the bin thiing
+    public bool blackBin;
+    private GameObject letterThing;
+    public int Score;
+
+    private void Awake()
+    {
+        letterThing = GameObject.FindGameObjectWithTag("Letter");
+    }
+    private void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (blackBin == true && letterThing.name == "blackLetter") 
+        {
+            Score += 1;
+        }
+    }
+}
