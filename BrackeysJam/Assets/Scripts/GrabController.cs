39e15a9c5ecbd9273826b0a2ca41ef83a18b38c4
V1660 <86119539+V1660@users.noreply.github.com>
2022-02-20 17:00:33 -0600

    I CAN PICK UP A BOX POGGERS

--- BrackeysJam/Assets/Scripts/GrabController.cs
+++ BrackeysJam/Assets/Scripts/GrabController.cs
@@ -6,6 +6,7 @@ public class GrabController : MonoBehaviour
 {
     public Transform grabDetect;
     public Transform boxHolder;
+    public bool holding;
     public float rayDist; 
     void Start()
     {
@@ -16,19 +17,24 @@ public class GrabController : MonoBehaviour
     void Update()
     {
         RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale, rayDist);
-
+        
         if (grabCheck.collider != null && grabCheck.collider.tag == "Box")
         {
-            if (Input.GetKey(KeyCode.G))
+            if (Input.GetKeyDown(KeyCode.E) && holding == false)
             {
+                holding = true;
                 grabCheck.collider.gameObject.transform.parent = boxHolder;
-                grabCheck.collider.gameObject.transform.position = boxHolder.position;
                 grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
             }
-            else
+            else if (Input.GetKeyDown(KeyCode.E) && holding == true)
             {
+                holding = false;
                 grabCheck.collider.gameObject.transform.parent = null;
                 grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            }
+            if (holding == true) 
+            {
+                grabCheck.collider.gameObject.transform.position = boxHolder.position;
             }
         }
     }
